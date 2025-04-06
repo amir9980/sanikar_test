a26d7ec32b403f4adb5ff80c5048e16de776499f
amir9980 <amir.9980@yahoo.com>
2025-04-06 18:58:24 +0330

    add date validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Nette\Schema\ValidationException;
 
 class TaskController extends Controller
 {
@@ -25,6 +26,10 @@ class TaskController extends Controller
             'end_date' => $request->end_date,
         ];
 
+        if(jdate($request->start_date) > jdate($request->end_date)){
+            throw new ValidationException('تاریخ شروع نمیتواند قبل از تاریخ پایان باشد.');
+        }
+
         auth()->user()->tasks()->create($task);
 
         return response()->json(['message' => 'با موفقیت ایحاد شد.']);
@@ -41,11 +46,11 @@ class TaskController extends Controller
     public function update(UpdateTaskRequest $request, Task $task)
     {
         $data = [
-            'title'=>$request->title,
-            'description'=>$request->description,
-            'completed'=>$request->completed,
-            'start_date'=>$request->start_date,
-            'end_date'=>$request->end_date,
+            'title' => $request->title,
+            'description' => $request->description,
+            'completed' => $request->completed,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
         ];
 
         $task->update($data);
